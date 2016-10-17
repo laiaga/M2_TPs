9b4a5392748bed0c7f14056b669ac8334ebda534
Alexandre Leonardi <leonardialexandre@free.fr>
2016-10-17 11:52:57 +0200

    correction d'n oubli d'add

---
+++ fiabilite/bilanFiabilite/src/implem/test/DateTest.java
@@ -0,0 +1,71 @@
+package implem.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void replaceTest() {
+		
+	}
+	
+	@Test
+	public void toOrdinalTest() {
+		
+	}
+	
+	@Test
+	public void weekDayTest() {
+		
+	}
+	
+	@Test
+	public void isoWeekDayTest() {
+		
+	}
+	
+	@Test
+	public void isoCalendarTest() {
+		
+	}
+	
+	@Test
+	public void isoFormatTest() {
+		
+	}
+	
+	@Test
+	public void toStringTest() {
+		
+	}
+	
+	@Test
+	public void ctimeTest() {
+		
+	}
+	
+	@Test
+	public void todayTest() {
+		
+	}
+
+	@Test
+	public void fromOrdinalTest() {
+		
+	}
+	
+	@Test
+	public void fromTimestampTest() {
+		
+	}
+}
